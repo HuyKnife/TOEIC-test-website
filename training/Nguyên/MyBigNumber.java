8595e5209e62b982608a501d1f18f8cb170dcf8a
shigoyabuki <43365723+shigoyabuki@users.noreply.github.com>
2018-12-18 23:27:13 +0700

    Updated the source code

--- training/Nguyên/MyBigNumber.java
+++ training/Nguyên/MyBigNumber.java
@@ -1,51 +1,79 @@
+/**
+ * Class cộng 2 số
+ */
 public class MyBigNumber {
+
+    /**
+     * Phương thức cộng 2 chuỗi số làm tham số cho hàm sum:
+     * 2 chuỗi này đều chỉ chứa các kí tự số từ '0' đến '9'.
+     *
+     * @param str1 chuỗi số thứ nhất.
+     * @param str2 chuỗi số thứ hai.
+     * @return chuỗi có giá trị là tổng của hai số str1 và str2.
+     */
+
+    private IObserver observer;
+
+    public MyBigNumber() {
+    }
+
+    public MyBigNumber(final IObserver observer) {
+        this.observer = observer;
+    }
+
     public String sum(String str1, String str2) {
 
-        String s1 = str1;
-        String s2 = str2;
-        String res = "";
-        int n1;
-        int n2;
+        int s1 = str1.length();                     //lấy ra độ dài của 2 chuỗi
+        int s2 = str2.length();
+        int digit1;                                 //biến chứa kí số trong chuỗi 1
+        int digit2;                                 //biến chưa kí số trong chuỗi 2
 
         int sum = 0;                                //khởi tạo biến tổng
         int rem = 0;                                //khởi tạo biến nhớ số dư
+        int write = 0;                              //khởi tạo biến chứa hàng đơn vị
+        int nho = 0;                                 //khởi tạo biến chứa số dư ban đầu
 
+        String res = "";                            //khởi tạo chuỗi chứa kết quả
+        String step = "";                           //khởi tạo chuỗi chứa bước phép cộng
 
-        if (s1.length() > s2.length()) {
-            while (s2.length() < s1.length())
-                s2 = "0" + s2;                      //cân bằng 2 chuỗi số bằng cách chèn số 0 phía trước
-        } else {
-            while (s1.length() < s2.length())
-                s1 = "0" + s1;
-        }
+        int length = (s1 > s2) ? s1 : s2;           //biến chứa độ dài lớn nhất giữa 2 chuỗi
 
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            n1 = s1.charAt(i) - '0';                //biến chuỗi 1 thành số nguyên theo bảng mã ascii
-            n2 = s2.charAt(i) - '0';                //biến chuỗi 2 thành số nguyên theo bảng mã ascii
-            sum = n1 + n2;
-            if (rem != 0) sum += rem;               //cộng số dư nếu có
-            rem = 0;                                //làm mới lại biến số dư
-            if (sum > 9) {
-                rem = sum / 10;                     //chia lấy số dư trường hợp tổng lớn hơn hoặc bằng 10
-                sum = sum % 10;                     //lấy chữ số bên trái điền vào chuỗi tổng
+        for (int i = 0; i < length; i++) {
+            digit1 = i < s1 ? (str1.charAt(s1 - i - 1) - '0') : 0;  // cập nhật vị trí của kí số và lấy kí số cuối
+            digit2 = i < s2 ? (str2.charAt(s2 - i - 1) - '0') : 0;
+
+            sum = digit1 + digit2;
+            write = (sum + rem) % 10;
+            if (sum > 10) {
+                nho = sum / 10;
             }
-            res += ("" + sum);                      //cộng số dư vào nếu còn sau khi chạy xong vòng lặp
+
+
+            if (length == 1) {
+                step += "\n" + "Lấy " + digit1
+                        + " cộng " + digit2
+                        + " bằng " + sum + "\n";
+            } else {
+                step += "\n" + "Bước" + (i + 1) + " :\n"
+                        + "Lấy " + digit1
+                        + " cộng cho " + digit2
+                        + " bằng " + sum + "\n"
+                        + "Cộng cho số dư là " + rem
+                        + " ta được số " + (sum + rem) + "\n"
+                        + "Viết " + write
+                        + " nhớ " + nho + "\n";
+            }
+
+            rem = sum / 10;
+            res = write + res;
         }
 
-        if (rem != 0) res += ("" + rem);            //
+        if (nho != 0) {
+            res = nho + res;
+        }
 
-        res = stringReverse(res);                     //đảo ngược chuỗi vì chuỗi được cộng từ phải sang trái
+        observer.sendStep(step);
 
         return res;
-    }
-
-
-    public static String stringReverse(String str) {  //hàm để đảo chuỗi
-        String tempt = "";
-        for (int i = str.length() - 1; i >= 0; i--) {
-            tempt += str.charAt(i);
-        }
-        str = tempt;
-        return str;
     }
 }
