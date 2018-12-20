ecdf58089af0ae852328429738cfa27677787929
shigoyabuki <43365723+shigoyabuki@users.noreply.github.com>
2018-12-20 23:19:36 +0700

    Update exceptions handling and fix logic errors

--- training/Nguyên/src/MyBigNumber.java
+++ training/Nguyên/src/MyBigNumber.java
@@ -1,16 +1,7 @@
 /**
- * Class sum of 2 string
+ * Class sum of 2 string.
  */
 public class MyBigNumber {
-
-    /**
-     * This class count the sum of 2 string number:
-     * These 2 strings only include number from '0' to '9'.
-     *
-     * @param str1 first string number.
-     * @param str2 second string number.
-     * @return string that have the sum number of these 2 strings.
-     */
 
     private IObserver observer;
 
@@ -21,16 +12,27 @@ public class MyBigNumber {
         this.observer = observer;
     }
 
-    public String sum(String str1, String str2) {
+    /**
+     * This class count the sum of 2 string number:
+     * These 2 strings only include number from '0' to '9'.
+     *
+     * @param s1 first string number.
+     * @param s2 second string number.
+     * @return string that have the sum number of these 2 strings.
+     */
 
-        // To check whether these 2 strings are included positive numbers or not
-        // All credit of this exception goes to Đào Anh Huy - 51600034
-        if (!str1.matches("^[0-9]+$") || !str2.matches("^[0-9]+$")) {
-            throw new NumberFormatException("The input must not contain word or negative number");
+    public String sum(final String s1, final String s2) {
+
+        if (s1.contains("-")) {
+            throw new NumberFormatException("Chuoi so 1 khong duoc co so am");
         }
 
-        int s1 = str1.length();                     //to get the length of 2 strings
-        int s2 = str2.length();
+        if (s2.contains("-")) {
+            throw new NumberFormatException("Chuoi so 2 khong duoc co so am");
+        }
+
+        int str1 = s1.length();                     //to get the length of 2 strings
+        int str2 = s2.length();
         int digit1;                                 //variable to store the digit of string 1
         int digit2;                                 //variable to store the digit of string 2
 
@@ -42,24 +44,47 @@ public class MyBigNumber {
         String res = "";                            //string to store the result
         String step = "";                           //string to store the steps
 
-        int length = (s1 > s2) ? s1 : s2;           //variable to store the longest length of 2 strings
+        char checker;                               //to check for words in the string
+
+        int length = (str1 > str2) ? str1 : str2;           //variable to store the longest length of 2 strings
 
         for (int i = 0; i < length; i++) {
-            digit1 = i < s1 ? (str1.charAt(s1 - i - 1) - '0') : 0;  // update and get the last digit
-            digit2 = i < s2 ? (str2.charAt(s2 - i - 1) - '0') : 0;
+            digit1 = i < str1 ? (s1.charAt(str1 - i - 1) - '0') : 0;  // update and get the last digit
+            digit2 = i < str2 ? (s2.charAt(str2 - i - 1) - '0') : 0;
+
+            checker = i < str1 ? s1.charAt(i) : '0';
+
+            if (!(checker >= '0' && checker <= '9')) {
+                throw new NumberFormatException("Vi tri thu "
+                        + (s1.indexOf(checker) + 1)
+                        + " cua chuoi so thu 1 khong phai la so");
+            }
+
+            checker = i < str2 ? s2.charAt(i) : '0';
+
+            if (!(checker >= '0' && checker <= '9')) {
+                throw new NumberFormatException("Vi tri thu "
+                        + (s2.indexOf(checker) + 1)
+                        + " cua chuoi so thu 2 khong phai la so");
+            }
 
             sum = digit1 + digit2;
             write = (sum + rem) % 10;
-            if (sum > 9) {
-                nho = sum / 10;
-            }
+            nho = sum / 10;
 
 
             if (length == 1) {
                 step += "\n" + "Lay " + digit1
                         + " cong " + digit2
                         + " bang " + sum + "\n";
-            } else if (i != 0) {
+            } else if (rem == 0) {
+                step += "\n" + "Buoc " + (i + 1) + " :\n"
+                        + "Lay " + digit1
+                        + " cong " + digit2
+                        + " duoc " + sum + "\n"
+                        + "Ghi " + write
+                        + " nho " + nho + "\n";
+            } else {
                 step += "\n" + "Buoc " + (i + 1) + " :\n"
                         + "Lay " + digit1
                         + " cong " + digit2
@@ -68,20 +93,13 @@ public class MyBigNumber {
                         + " ta duoc " + (sum + rem) + "\n"
                         + "Ghi " + write
                         + " nho " + nho + "\n";
-            } else {
-                step += "\n" + "Buoc " + (i + 1) + " :\n"
-                        + "Lay " + digit1
-                        + " cong " + digit2
-                        + " duoc " + sum + "\n"
-                        + "Ghi " + write
-                        + " nho " + nho + "\n";
             }
 
             rem = sum / 10;
             res = write + res;
         }
 
-        if (nho != 0) {                             //If after all the calculation still have the carry number, add it to the result
+        if (nho != 0) {                             //Add the carry number if still have any
             res = nho + res;
         }
 
